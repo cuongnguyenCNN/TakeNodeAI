460e787b886bdf32d6f32a8f0c26af041bb5397c
Nguyễn Ngọc Cương <cuongnguyen71195@gmail.com>
2025-04-25 15:43:33 +0700

    gagag

--- src/app/components/flashcard.tsx
+++ src/app/components/flashcard.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { useState } from "react";
 import styles from "./Flashcard.module.css";
 const initialCards = [
--- src/app/dashboard/notes/layout.tsx
+++ src/app/dashboard/notes/layout.tsx
@@ -3,5 +3,16 @@ export default function NoteLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return <section>{children}</section>;
+  return (
+    <section>
+      {" "}
+      <div className="transition-all w-full h-full  flex flex-col  duration-400 pl-[272px] max-[866px]:pl-0 max-tablet:pl-0">
+        <div className="flex w-full flex-col px-8 max-[600px]:px-4 !flex-row pl-0">
+          <div className="w-full ml-8 max-[600px]:ml-0 h-auto">
+            <div className="flex flex-col  space-y-3  pt-5 ">{children}</div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
